c7e87e0dd5809cfc6dbaa5bf29ffd6a83030fc62
Ankit Mishra <52539977+ankitmishra7@users.noreply.github.com>
2020-05-17 04:22:44 +0530

    Final Version

--- app/src/main/java/com/example/bitactrocalculator/MainActivity.java
+++ app/src/main/java/com/example/bitactrocalculator/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.bitactrocalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button button0, button1,button2,button3,button4,button5,button6,button7,button8,button9,buttonAdd,buttonSub,buttonDivision,buttonMul,button10,buttonC,buttonEqual;
-    EditText bitactroEditText;
+    EditText bitactroEditText,resultEditText;
+    ImageView clear;
     float mValueOne,mValueTwo;
+    double store,val3,result=0;
     boolean bitactroAddition,mSubtract,bitactroMultiplication,bitactroDivision;
-     int val1=0,val2;
+    String str="";
+    int val1=0,val2,tcount=0,acount=0,scount=0,dcount=0,mcount=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +44,23 @@ public class MainActivity extends AppCompatActivity {
         buttonDivision= (Button) findViewById(R.id.buttonDivision);
         buttonC= (Button) findViewById(R.id.buttonC);
         buttonEqual= (Button) findViewById(R.id.buttonEqual);
+        clear=(ImageView)findViewById(R.id.cut);
+
         bitactroEditText= (EditText) findViewById(R.id.edt1);
+        resultEditText= (EditText) findViewById(R.id.edt2);
 
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                 bitactroEditText.setText(bitactroEditText.getText()+"1");
-                 
+                 str=str+'1';
+                 bitactroEditText.setText(str);
+                 String ans=calculate(str);
+                 resultEditText.setText(ans);
+
+
+
+
+
 
             }
         });
@@ -49,7 +68,12 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                bitactroEditText.setText(bitactroEditText.getText()+"2");
+                str=str+"2";
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
+
 
             }
         });
@@ -57,48 +81,76 @@ public class MainActivity extends AppCompatActivity {
         button3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                bitactroEditText.setText(bitactroEditText.getText()+"3");
+                str=str+"3";
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
 
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                bitactroEditText.setText(bitactroEditText.getText()+"4");
+                str=str+"4";
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
 
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bitactroEditText.setText(bitactroEditText.getText()+"5");
+                str=str+'5';
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
 
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bitactroEditText.setText(bitactroEditText.getText()+"6");
+                str=str+"6";
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
 
         button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bitactroEditText.setText(bitactroEditText.getText()+"7");
+                str=str+"7";
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bitactroEditText.setText(bitactroEditText.getText()+"8");
+                str=str+"8";
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
 
         button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bitactroEditText.setText(bitactroEditText.getText()+"9");
+                str=str+"9";
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
 
@@ -109,75 +161,123 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(bitactroEditText==null)
-                    bitactroEditText.setText("");
-                else {
-                    bitactroEditText.setText(bitactroEditText.getText()+"+");
-                    String text=bitactroEditText.getText().toString();
+                if(str.length()==0)
+                {
+                    bitactroEditText.setText("Bad Expression");
+                }
+                else if(validCheck(str)) {
 
+                    bitactroEditText.setText(str+ "+");
+                    str=str+'+';
+                }
+                else
+                {
 
-                    val1=text.length();
-                    bitactroAddition = true;
+                    if(str.length()>0)
+                    {
 
+                        str=str.substring(0,str.length()-1);
+                        str=str+"+";
+                        bitactroEditText.setText(str);
+                    }
+                    else  bitactroEditText.setText("Bad Expression");
+                }
 
                 }
-            }
         });
 
 
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bitactroEditText.setText(bitactroEditText.getText()+"0");
+                str+='0';
+                bitactroEditText.setText(str);
+                String ans=calculate(str);
+                resultEditText.setText(ans);
+
             }
         });
 
         buttonSub.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                if(bitactroEditText==null)
-                    bitactroEditText.setText("");
-                else {
+                if(str.length()==0)
+                {
+                    bitactroEditText.setText(str+"-");
+                    str+='-';
+                }
+                else if(str.length()>1&&str.charAt(str.length() - 1) == '-') {
+
+                }
+                else if(validCheck(str)) {
+
                     bitactroEditText.setText(bitactroEditText.getText() + "-");
-                    String text = bitactroEditText.getText().toString();
+                    str=str+'-';
+                }
+                else
+                {
+                    int len=bitactroEditText.getText().length();
+                    if(len>0)
+                    {
+                        if(str.length()>1&&str.charAt(str.length()-1)=='+')
+                              str=str.substring(0,str.length()-1);
+                        str=str+"-";
+                        bitactroEditText.setText(str);
+                    }
+                }
 
 
-                    val1 = text.length();
-                    mSubtract = true;
-                    //bitactroEditText.setText(null);
-                }}
+                }
         });
 
         buttonMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(bitactroEditText==null)
-                    bitactroEditText.setText("");
-                else {
-                    bitactroEditText.setText(bitactroEditText.getText() + "*");
-                    String text = bitactroEditText.getText().toString();
 
-
-                    val1 = text.length();
-                    bitactroMultiplication = true;
-                    //bitactroEditText.setText(null);
+                if(str.length()==0)
+                {
+                    bitactroEditText.setText("Bad Expression");
                 }
+                else if(validCheck(str)) {
+
+                    bitactroEditText.setText(bitactroEditText.getText() + "*");
+                    str=str+'*';
+                }
+                else
+                {
+                    int len=bitactroEditText.getText().length();
+                    if(len>0)
+                    {
+                        str=str.substring(0,str.length()-1);
+                        str=str+"*";
+                        bitactroEditText.setText(str);
+                    }
+                }
+
             }
         });
 
         buttonDivision.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(bitactroEditText==null)
-                    bitactroEditText.setText("");
-                else {
+                if(str.length()==0)
+                {
+                    bitactroEditText.setText("Bad Expression");
+                }
+                else if(validCheck(str)) {
+
                     bitactroEditText.setText(bitactroEditText.getText() + "/");
-                    String text = bitactroEditText.getText().toString();
-
-
-                    val1 = text.length();
-                    bitactroDivision = true;
-
+                    str=str+'/';
+                }
+                else
+                {
+                    int len=bitactroEditText.getText().length();
+                    if(len>0)
+                    {
+                        str=str.substring(0,str.length()-1);
+                        str=str+"/";
+                        bitactroEditText.setText(str);
+                    }
                 }
             }
         });
@@ -186,67 +286,76 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+          str=bitactroEditText.getText().toString();
+          String ans=calculate(str);
+          resultEditText.setText(ans);
 
-                if(bitactroAddition==true){
-                    String text=bitactroEditText.getText().toString();
-                    val2=text.length();
-                    mValueOne=Float.parseFloat(text.substring(0,val1-1));
-                    mValueTwo = Float.parseFloat(text.substring(val1,val2));
-
-                    float result =mValueTwo+mValueOne;
-                    bitactroEditText.setText(null);
-                    bitactroEditText.setText(result+"");
-                    bitactroAddition=false;
-                }
-                if(mSubtract==true){
-                    String text=bitactroEditText.getText().toString();
-
-                    val2=text.length();
-                    mValueOne=Float.parseFloat(text.substring(0,val1-1));
-                    mValueTwo = Float.parseFloat(text.substring(val1,val2));
-
-                    float result =mValueOne-mValueTwo;
-                    bitactroEditText.setText(null);
-                    bitactroEditText.setText(result+"");
-                    mSubtract=false;
-                }
-                if(bitactroMultiplication==true){
-                    String text=bitactroEditText.getText().toString();
-                    val2=text.length();
-                    mValueOne=Float.parseFloat(text.substring(0,val1-1));
-                    mValueTwo = Float.parseFloat(text.substring(val1,val2));
-
-                    float result =mValueTwo*mValueOne;
-                    bitactroEditText.setText(null);
-                    bitactroEditText.setText(result+"");
-                    bitactroMultiplication=false;
-                }
-                if(bitactroDivision==true){
-                    String text=bitactroEditText.getText().toString();
-                    val2=text.length();
-                    mValueOne=Float.parseFloat(text.substring(0,val1-1));
-                    mValueTwo = Float.parseFloat(text.substring(val1,val2));
-
-                    float result =mValueOne/mValueTwo;
-                    bitactroEditText.setText(null);
-                    bitactroEditText.setText(result+"");
-                    bitactroDivision=false;
-                }
             }
         });
 
         buttonC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bitactroEditText.setText("");
+                str="";
+                bitactroEditText.setText(str);
+                resultEditText.setText(str);
             }
         });
 
         button10.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                bitactroEditText.setText(bitactroEditText.getText()+".");
+                bitactroEditText.setText(str+".");
             }
         });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(str.length()>0)
+                   str=str.substring(0,str.length()-1);
+                bitactroEditText.setText(str);
+                String ans="";
+                if(str.length()>0) {
+                    ans = calculate(str);
+                }
+                    resultEditText.setText(ans);
+
+
+            }
+        });
+    }
+
+    boolean validCheck(String S)
+    {
+        if(S==null)
+            return false;
+        else if(S.charAt(S.length()-1)=='/'||S.charAt(S.length()-1)=='*'||S.charAt(S.length()-1)=='+'||S.charAt(S.length()-1)=='-')
+            return false;
+
+        return true;
+
+
+    }
+    String calculate(String S)   {
+        String res="";
+
+        char c=S.charAt(S.length()-1);
+        String exp;
+        if(c=='+'||c=='-'||c=='*'||c=='/')
+        {
+            exp=S.substring(0,S.length()-1);
+        }
+        else exp=S;
+        ScriptEngine engine=new ScriptEngineManager().getEngineByName("rhino");
+        Object result= null;
+        try {
+            result = engine.eval(exp);
+            res=result.toString();
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+
+        return res;
     }
 }
